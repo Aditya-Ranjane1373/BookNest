014fb75c81d3e4d31576ccb36dc282fdcc96491b
Aditya Ranjane <adityaranjane1373@gmail.com>
2025-07-17 18:56:59 +0530

    Add files via upload

---
+++ BookNestApplication.java
@@ -0,0 +1,14 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookNestApplication.class, args);
+		System.out.println("<-------------Hotel Application Run------------------->");
+	}
+
+}
